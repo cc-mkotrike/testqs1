8505849452309c989fe708878971d8e12ef70077
cc-sshaik <shaharaz.shaik@corecompete.com>
2020-09-30 19:31:25 +0530

    Fixed AD Install with quotes

--- scripts/sas_clients.ps1
+++ scripts/sas_clients.ps1
@@ -69,9 +69,9 @@ $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http:
 
 #Install AD DS, DNS and GPMC
 start-job -Name addFeature -ScriptBlock {
-Add-WindowsFeature -Name “ad-domain-services” -IncludeAllSubFeature -IncludeManagementTools
-Add-WindowsFeature -Name “dns” -IncludeAllSubFeature -IncludeManagementTools
-Add-WindowsFeature -Name “gpmc” -IncludeAllSubFeature -IncludeManagementTools }
+Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools
+Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools
+Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools }
 Wait-Job -Name addFeature
 #Get-WindowsFeature | Where installed >>$featureLogPath
 
